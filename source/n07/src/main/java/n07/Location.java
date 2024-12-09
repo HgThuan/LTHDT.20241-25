dc8c87b3cb8aac250d5b7ddb525638d52996d084
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2024-12-09 16:04:43 +0700

    Update class Location

--- source/n07/src/main/java/n07/Location.java
+++ source/n07/src/main/java/n07/Location.java
@@ -4,35 +4,42 @@ public class Location {
     public int x;
     public int y;
 
+    // Khởi tạo vị trí tại (x, y)
     public Location(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    // Khởi tạo vị trí tại (0, 0)
     public Location() {
         this.x = 0;
         this.y = 0;
     }
 
+    // Đặt vị trí mới cho đối tượng
     public void setLocation(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    // Đặt vị trí mới cho đối tượng
     public void setLocation(Location location) {
         this.x = location.x;
         this.y = location.y;
     }
 
+    // Lấy vị trí hiện tại
     public Location getLocation() {
         return new Location(this.x, this.y);
     }
 
+    // Di chuyển đối tượng theo x, y
     public void move(int x, int y) {
         this.x += x;
         this.y += y;
     }
 
+    // Di chuyển đối tượng theo hướng và khoảng cách
     public void moveDirection(int direction, int distance) {
         int moveX = distance * (int) Math.cos(Math.toRadians(direction));
         int moveY = distance * (int) Math.sin(Math.toRadians(direction));
