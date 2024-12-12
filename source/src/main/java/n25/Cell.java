5481ffdc56b9e2cc6584f2ba0caf96e2ecf22635
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2024-12-12 09:50:09 +0700

    Sửa lại lớp cell 😠

--- source/src/main/java/n25/Cell.java
+++ source/src/main/java/n25/Cell.java
@@ -1,41 +1,52 @@
 package n25;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
+import javafx.scene.shape.Shape;
 
 public class Cell {
-    private Location location;      // Vị trí của tế bào
-    private int unitSize;           // Kích thước tế bào
-    private Color color;            // Màu sắc của tế bào
-    private boolean infected;       // Trạng thái tế bào bị nhiễm hay chưa
-    private Circle cellShape;       // Đối tượng hình tròn đại diện cho tế bào
-    private Text statusText;        // Hiển thị trạng thái của tế bào
+    private Location location;                          // Vị trí của tế bào
+    private int radius;                                 // Bán kính của tế bào
+    private int unitSize;                               // Kích thước tế bào
+    private Color color;                                // Màu sắc của tế bào
+    private Color subColor;                             // Màu sắc của tế bào khi bị nhiễm
+    private List<Shape> shapes = new ArrayList<>();     // Danh sách các Shape để vẽ tế bào
 
-    public Cell(Location location, int unitSize, Color color) {
+    public Cell(Location location, int radius, int unitSize, Color color, Color subColor) {
         this.location = location;
+        this.radius = radius;
         this.unitSize = unitSize;
         this.color = color;
-        this.infected = false;
+        this.subColor = subColor;
     }
 
-    public void draw(Pane pane) {
-        cellShape = new Circle(location.x, location.y, unitSize * 5);
-        cellShape.setFill(color);
-        cellShape.setStroke(Color.BLACK); // set màu viền
-        cellShape.setStrokeWidth(2);
+    public void draw(Pane area) {
+        for (Shape shape : shapes) {
+            area.getChildren().remove(shape);
+        }
+        shapes.clear();
 
-        statusText = new Text(location.x - unitSize, location.y - unitSize - 10, "Healthy");
-        statusText.setFill(Color.BLACK);
-
-        pane.getChildren().addAll(cellShape, statusText);
+        ComponentStyle.createComponent(ComponentStyle.CIRCLE_STYLE, shapes, location, radius, unitSize, color);
+        
+        if (shapes.size() > 0) {
+            for (Shape shape : shapes) {
+                area.getChildren().add(shape);
+            }
+        }
     }
 
-    public void showInfection(Pane pane, Virus virus) {
-        if (infected) {
-            cellShape.setFill(Color.RED);
-            statusText.setText("Infected by " + virus.getName());
-        } 
+    public void beInfected() {
+        for (Shape shape : shapes) {
+            shape.setFill(subColor);
+        }
+    }
+
+    public void recover() {
+        for (Shape shape : shapes) {
+            shape.setFill(color);
+        }
     }
 }
