8b8e9be5a9a9e7538f346b2d6d4053bce15ac7ce
Ly Tien <lycongtien1312004@gmail.com>
2024-12-17 17:15:18 +0700

    Update nucleoid

--- source/src/main/java/n25/Nucleoid.java
+++ source/src/main/java/n25/Nucleoid.java
@@ -3,7 +3,6 @@ package n25;
 import javafx.scene.paint.Color;
 
 public class Nucleoid extends VirusComponent {
-    int granularity;
     public Nucleoid(Location center, int radius, int unitSize, Color color) {
         super(center, radius, unitSize, color, Color.BLACK);
         this.componentStyle = ComponentStyle.SPIRAL_STYLE;
